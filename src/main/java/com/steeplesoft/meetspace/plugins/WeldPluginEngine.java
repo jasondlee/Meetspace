7c05faaef3da0195d04c164d5fe27a3caecd32c1
Jason Lee <jason@steeplesoft.com>
2010-02-15 11:13:07 -0600

    Adding WeldPluginEngine and tests.  It doesn't do anything yet, but I'm working with the Weld guys on it

---
+++ src/main/java/com/steeplesoft/meetspace/plugins/WeldPluginEngine.java
@@ -0,0 +1,29 @@
+package com.steeplesoft.meetspace.plugins;
+
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jasonlee
+ * Date: Feb 14, 2010
+ * Time: 10:46:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WeldPluginEngine extends PluginEngine {
+    public WeldPluginEngine(String pluginDir) {
+        super(pluginDir);
+    }
+
+    public WeldPluginEngine(String pluginDir, String pkg) {
+        super(pluginDir, pkg);
+    }
+
+    public WeldPluginEngine(String pluginDir, Set<String> packages) {
+        super(pluginDir, packages);
+    }
+
+    @Override
+    protected void postProcessClass(Class<?> c) {
+        System.out.println("Passing " + c.getName() + " to Weld.");
+    }
+}
---
+++ src/test/java/com/steeplesoft/meetspace/test/WeldPluginEngineTest.java
@@ -0,0 +1,13 @@
+package com.steeplesoft.meetspace.test;
+
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jasonlee
+ * Date: Feb 14, 2010
+ * Time: 10:49:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WeldPluginEngineTest extends PluginEngineTest {
+}
